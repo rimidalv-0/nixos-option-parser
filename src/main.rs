ee3ca0ff6650b28aa86c051eafd3af179b6d4b76
compl1cated <vladimor.chak@gmail.com>
2025-10-04 21:21:36 +0200

    yeah more or less works, not the best format, but its rust project not a design one

--- src/main.rs
+++ src/main.rs
@@ -5,109 +5,97 @@ use std::io::{self, Write};
 use std::path::PathBuf;
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
+	if args.len() != 3 {
+		eprintln!(
+			"Usage: {} <path-to-file || json-object> <path-to_dir>",
+			&args[0]
+		);
+		std::process::exit(1);
+	}
 
-    if args.len() != 3 {
-        eprintln!(
-            "Usage: {} <path-to-file || json-object> <path-to_dir>",
-            &args[0]
-        );
-        std::process::exit(1);
-    }
+	let input_file: String = fs::read_to_string(&args[1])?;
+	let target_dir: PathBuf = PathBuf::from(&args[2]);
+	let input_json: Map<String, Value> = serde_json::from_str(&input_file)?;
 
-    let input_file: String = fs::read_to_string(&args[1])?;
-
-    let target_dir: PathBuf = PathBuf::from(&args[2]);
-
-    let input_json: Map<String, Value> = serde_json::from_str(&input_file)?;
-
-    for (k, v) in input_json {
-        let mut file_path = PathBuf::from(&target_dir);
-        for part in k.split('.') {
-            file_path.push(part);
-        }
-        file_path.set_extension("md");
-
-        let file_dir: PathBuf = PathBuf::from(&file_path.parent().unwrap());
-        fs::create_dir_all(file_dir)?;
-
-        let mut file = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open(file_path)?;
-
-        writeln!(file, "- Full option name\n ```nix\n {k}\n ```")?;
-        write_option(&mut file, v, 1);
-    }
-
-    return Ok(());
+	for (k, v) in input_json {
+		let mut file_path = PathBuf::from(&target_dir);
+		for part in k.split('.') {
+			file_path.push(part);
+		}
+		file_path.set_extension("md");
+		let file_dir: PathBuf = PathBuf::from(&file_path.parent().unwrap());
+		fs::create_dir_all(file_dir)?;
+		let mut file = OpenOptions::new()
+			.create(true)
+			.append(true)
+			.open(file_path)?;
+		writeln!(file, "- Name\n ```nix\n {k}\n ```")?;
+		write_option(&mut file, v, 0)?;
+	}
+	return Ok(());
 }
 
 fn indent(level: u8) -> String {
-    let mut str = String::new();
-    for _ in 0..level {
-        str.push(' ');
-    }
-    str
+	let mut str = String::new();
+	for _ in 0..level {
+		str.push('\t');
+	}
+	return str;
 }
 
-fn write_option(file: &mut File, value: Value, level: u8) {
-    match value {
-        Value::Array(a) => {
-            write!(file, "[");
-            for v in a {
-                write_option(file, v, level + 1);
-            }
-            write!(file, "]");
-        }
-        Value::Bool(b) => match b {
-            true => {
-                write!(file, "- [x] ");
-            }
-            false => {
-                write!(file, "- [] ");
-            }
-        },
-        Value::Null => {
-            write!(file, "NULL");
-        }
-        Value::Number(n) => {
-            write!(file, "{}", n);
-        }
-        Value::Object(o) => {
-            for (k, v) in o {
-                write!(file, "{}- {}:", indent(level), k);
-                match &v {
-                    Value::Array(_) => {
-                        write!(file, "\n{}[\n", indent(level));
-                        write_option(file, v, level + 1);
-                        write!(file, "\n{}]\n", indent(level));
-                    }
-                    Value::Bool(_) => {
-                        write!(file, " ");
-                        write_option(file, v, level);
-                    }
-                    Value::Null => {}
-                    Value::Number(_) => {
-                        write!(file, " ");
-                        write_option(file, v, level);
-                        write!(file, "\n");
-                    }
-                    Value::Object(_) => {
-                        write!(file, " {{\n");
-                        write_option(file, v, level + 1);
-                        write!(file, "\n{}}}\n", indent(level));
-                    }
-                    Value::String(_) => {
-                        write!(file, " ");
-                        write_option(file, v, level);
-                        write!(file, "\n");
-                    }
-                }
-            }
-        }
-        Value::String(s) => {
-            write!(file, "{}", s.replace("\n", " "));
-        }
-    }
+fn write_option(file: &mut File, value: Value, level: u8) -> io::Result<()> {
+	match value {
+		Value::Array(a) => {
+			for v in a {
+				write_option(file, v, level)?;
+			}
+		}
+		Value::Bool(b) => {
+			write!(file, " {}\n", b)?;
+		}
+		Value::Null => {
+			write!(file, " NULL\n")?;
+		}
+		Value::Number(n) => {
+			write!(file, " {}\n", n)?;
+		}
+		Value::Object(o) => {
+			for (k, v) in o {
+				write!(file, "{}- **{}:**", indent(level), k)?;
+				match &v {
+					Value::Array(_) => {
+						write!(file, " [\n")?;
+						write_option(file, v, level + 1)?;
+						write!(file, "{}]\n", indent(level + 1))?;
+					}
+					Value::Bool(_) => {
+						write!(file, " ")?;
+						write_option(file, v, 0)?;
+					}
+					Value::Null => {
+						write!(file, " ")?;
+						write_option(file, v, 0)?;
+					}
+					Value::Number(_) => {
+						write!(file, " ")?;
+						write_option(file, v, 0)?;
+					}
+					Value::Object(_) => {
+						write!(file, " {{\n")?;
+						write_option(file, v, level + 1)?;
+						write!(file, "{}}}\n", indent(level + 1))?;
+					}
+					Value::String(_) => {
+						write!(file, " ")?;
+						write_option(file, v, 0)?;
+					}
+				}
+			}
+		}
+		Value::String(s) => {
+			write!(file, " {}\n", s.replace("\n", " "))?;
+		}
+	}
+	return Ok(());
 }
